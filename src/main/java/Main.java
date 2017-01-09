de306ddf5e9167ac9d4590fc4c565c07757ddcc0
UnusualArnen <jack@skroper.com>
2017-01-09 06:58:12 -0700

    SQLite basic stuff added to main file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,12 +11,25 @@ public class Main {
         Connection c = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c = DriverManager.getConnection("jdbc:sqlite:subs.db");
+            System.out.println("Opened database successfully");
+            Statement statement = c.createStatement();
+
+            statement.executeUpdate("DROP TABLE IF EXISTS SUBS");
+            statement.executeUpdate("create table subs (id integer, operator string)");
+            statement.executeUpdate("INSERT INTO SUBS VALUES(1 , 'OR')");
+            ResultSet rs = statement.executeQuery("SELECT * FROM SUBS");
+            while (rs.next()) {
+                System.out.println(rs.getString("operator"));
+            }
+        } catch(SQLException e) {
+                // if the error message is "out of memory",
+                // it probably means no database file is found
+            System.err.println(e.getMessage());
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 
 }
